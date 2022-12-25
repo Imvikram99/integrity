f9e42104dafc4791b34db0a3c82129375e1600ce
shahid-106 <shahid106@outlook.com>
2022-12-25 23:01:15 +0500

    bug fixes

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,6 @@ class Constants{
   static Color appTextColor=Color(0xff09585a);
   static Color appButtonColor=Color(0xff13aeff);
   static String appName='Home';
+  static String cashFreeOrderStage="TEST"; //PROD;
+  static String cashFreeNotifyUrl="https://webhook.site/472fc2d6-eb67-48e8-ab69-15cedd664015";
 }
